97722109d4547e3519864bccaa896deffcf62ca1
James Ferguson <ncjamesferguson@gmail.com>
2017-12-01 01:41:23 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,9 @@
 def reverse_each_word(string)
 
-  i = 0
   arr = string.split(" ")
-  arr2 = []
-  arr.each do |word|
-    arr2[i] = word.reverse
-    i =+ 1
-  end
 
-  arr2.join(" ")
+  arr.collect do |word|
+    arr << word.reverse
+  end
 
 end
